f1272784854a68f84d8f5a6129d7fc63da6d901b
Devin Cunningham <devincunningham@allegropediatrics.com>
2017-09-28 12:03:37 -0700

    updated js on desktop

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ alert('Welcome to my guessing game!');
 var user = prompt('What is your name?');
 alert('Hi ' + user + '! I am so happy you want to play my game!');
 
-var answer = prompt('Is Devin\'s favorite food Chipotle? Type yes or no').toLowercase();
+var answer = prompt('Is Devin\'s favorite food Chipotle? Type yes or no').toLowerCase();
 console.log(answer);
 
 if (answer === 'yes' || answer === 'y'){
   userPoints++;
 } else {
-  alert('Wrong! Actually, I like all mexican food!!');
+  alert('Wrong! Actually, I LOVE mexican food!!');
 }
 
-alert('You have' + userPoints + ' points!');
+alert('You have ' + userPoints + ' point(s)!');
+console.log(userPoints);
